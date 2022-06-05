a1722797b0400f26e09fc818a44b622671339e6d
fbtwitter <rzaugusdi354@gmail.com>
2022-06-05 15:52:31 +0700

    customizing prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    semi: ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['error'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+}
